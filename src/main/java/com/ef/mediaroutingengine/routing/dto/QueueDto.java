c38cc23d88b1fd4010c04afe72a1658032c28487
muhammad zubair <muhammad.zubair@expertflow.com>
2023-08-03 22:10:44 +0500

    Add default constructor

--- src/main/java/com/ef/mediaroutingengine/routing/dto/QueueDto.java
+++ src/main/java/com/ef/mediaroutingengine/routing/dto/QueueDto.java
@@ -2,12 +2,14 @@ package com.ef.mediaroutingengine.routing.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * The Queue DTO.
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class QueueDto {
     private String queueId;
     private String queueName;
