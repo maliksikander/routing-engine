1db87add0ef5cb5e651bb9931f40a9195191debc
muhammad zubair <muhammad.zubair@expertflow.com>
2023-08-04 15:05:19 +0500

    Add unit tests for TaskService class

--- src/test/java/com/ef/mediaroutingengine/taskmanager/service/TasksServiceTest.java
+++ src/test/java/com/ef/mediaroutingengine/taskmanager/service/TasksServiceTest.java
@@ -1,10 +1,11 @@
-package com.ef.mediaroutingengine.taskmanager.service.taskservice;
+package com.ef.mediaroutingengine.taskmanager.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import com.ef.cim.objectmodel.ChannelSession;
 import com.ef.cim.objectmodel.Enums;
 import com.ef.cim.objectmodel.MediaRoutingDomain;
@@ -14,21 +15,31 @@ import com.ef.cim.objectmodel.TaskType;
 import com.ef.cim.objectmodel.dto.TaskDto;
 import com.ef.mediaroutingengine.global.exceptions.NotFoundException;
 import com.ef.mediaroutingengine.global.jms.JmsCommunicator;
+import com.ef.mediaroutingengine.routing.dto.QueueDto;
+import com.ef.mediaroutingengine.routing.dto.QueueHistoricalStats;
+import com.ef.mediaroutingengine.routing.model.PrecisionQueue;
 import com.ef.mediaroutingengine.routing.pool.PrecisionQueuesPool;
 import com.ef.mediaroutingengine.routing.utility.RestRequest;
+import com.ef.mediaroutingengine.taskmanager.dto.TaskEwtAndPositionResponse;
 import com.ef.mediaroutingengine.taskmanager.model.Task;
 import com.ef.mediaroutingengine.taskmanager.pool.TasksPool;
 import com.ef.mediaroutingengine.taskmanager.repository.TasksRepository;
-import com.ef.mediaroutingengine.taskmanager.service.TasksService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.Collections;
+import com.ef.mediaroutingengine.taskmanager.service.taskservice.TasksRetriever;
+import com.ef.mediaroutingengine.taskmanager.service.taskservice.TasksRetrieverFactory;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @ExtendWith(MockitoExtension.class)
 class TasksServiceTest {
@@ -93,23 +104,22 @@ class TasksServiceTest {
     }
 
     @Test
-    void TestTaskPosition()
-    {
+    void TestTaskPosition() {
         List<Task> taskList = new ArrayList<>();
         TaskState taskState = new TaskState(Enums.TaskStateName.QUEUED, null);
-        TaskType type = new TaskType(Enums.TaskTypeDirection.INBOUND, Enums.TaskTypeMode.QUEUE,null);
+        TaskType type = new TaskType(Enums.TaskTypeDirection.INBOUND, Enums.TaskTypeMode.QUEUE, null);
         TaskQueue taskQueue = new TaskQueue(UUID.randomUUID().toString(), "Chat");
         for (int i = 1; i <= 10; i++) {
             taskList.add(Task.getInstanceFrom(
                     getNewChannelSession(),
-                    getNewMrd(),
+                    getMrd(),
                     taskQueue,
                     taskState,
                     type,
                     i
             ));
         }
-        Task task = Task.getInstanceFrom(getNewChannelSession(), getNewMrd(), taskQueue, taskState,type, 4);
+        Task task = Task.getInstanceFrom(getNewChannelSession(), getMrd(), taskQueue, taskState, type, 4);
         taskList.add(task);
         when(tasksPool.findByQueueId(taskQueue.getId())).thenReturn(taskList);
         int checkTask = tasksService.getTaskPosition(task);
@@ -119,6 +129,52 @@ class TasksServiceTest {
         });
     }
 
+    @Nested
+    @DisplayName("Ewt and Position Tests")
+    class GetEwtAndPosition_Tests {
+        @Test
+        void whenNoQueuedTaskExist() {
+            String conversationId = UUID.randomUUID().toString();
+            List<Task> queuedTask = new ArrayList<>();
+            when(tasksPool.findQueuedTasksFor(conversationId)).thenReturn(Collections.EMPTY_LIST);
+
+            ResponseEntity<Object> response = tasksService.getTaskEwtAndPosition(conversationId);
+
+            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+            assertThat(response.getBody()).isInstanceOf(List.class);
+            verifyNoMoreInteractions(queuePool);
+        }
+    }
+
+    @Nested
+    @DisplayName("Tests for calculating EWT and Position")
+    class CalculateEwtAndPosition {
+        @Test
+        void test_whenAverageHandleTimeIsZero() {
+            Task task = getTask(Enums.TaskStateName.QUEUED, Enums.TaskTypeDirection.INBOUND, Enums.TaskTypeMode.QUEUE);
+            TasksService mockInstance = mock(TasksService.class);
+            PrecisionQueue precisionQueue = mock(PrecisionQueue.class);
+            QueueHistoricalStats queueHistoricalStats = new QueueHistoricalStats();
+            QueueDto queueDto = new QueueDto(UUID.randomUUID().toString(), "chat");
+            queueHistoricalStats.setQueue(queueDto);
+            queueHistoricalStats.setAverageHandleTime(0);
+            queueHistoricalStats.setAverageWaitTime(0);
+            List<Task> tasksInQueue = new ArrayList<>();
+            tasksInQueue.add(task);
+
+            when(tasksPool.findByQueueId(task.getQueue().getId())).thenReturn(tasksInQueue);
+            when(queuePool.findById(task.getQueue().getId())).thenReturn(precisionQueue);
+            when(restRequest.getQueueHistoricalStats(task.getQueue().getId())).thenReturn(queueHistoricalStats);
+            when(precisionQueue.getAssociatedAgents()).thenReturn(Collections.EMPTY_LIST);
+
+
+            TaskEwtAndPositionResponse response = tasksService.calculateTaskEwtAndPosition(task);
+
+            assertThat(response.getPosition()).isEqualTo(1);
+            assertThat(response.getEwt()).isEqualTo(5);
+        }
+    }
+
     private ChannelSession getNewChannelSession() {
         ChannelSession channelSession = new ChannelSession();
         channelSession.setId(UUID.randomUUID().toString());
@@ -126,11 +182,19 @@ class TasksServiceTest {
         return channelSession;
     }
 
-    private MediaRoutingDomain getNewMrd() {
+    private MediaRoutingDomain getMrd() {
         MediaRoutingDomain mrd = new MediaRoutingDomain();
         mrd.setId(UUID.randomUUID().toString());
         mrd.setName("Chat");
         mrd.setDescription("Description");
         return mrd;
+    }
+
+    private Task getTask(Enums.TaskStateName state, Enums.TaskTypeDirection direction, Enums.TaskTypeMode mode) {
+        TaskState taskState = new TaskState(state, null);
+        TaskType type = new TaskType(direction, mode, null);
+        TaskQueue taskQueue = new TaskQueue(UUID.randomUUID().toString(), "Chat");
+        Task task = Task.getInstanceFrom(getNewChannelSession(), getMrd(), taskQueue, taskState, type, 4);
+        return task;
     }
 }
